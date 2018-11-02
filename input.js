514388fd834d17ad13c61c8f1fb9f0025198b466
hoangminhtri <hoangminhtri@codelovers.vn>
2018-11-02 21:15:42 +0700

    Sokoban Javascript

---
+++ input.js
@@ -0,0 +1,523 @@
+/**
+ * Created by hoangminhtri on 22/03/2017.
+ */
+var json = {
+    "listPoints": [
+        {
+            "row": 1, //person
+            "point": {
+                "x": 0,
+                "y": 5,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 0
+            }
+        },
+        {
+            "row": 2,
+            "point": {
+                "x": 1,
+                "y": 5,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 0
+            }
+        },
+        {
+            "row": 3,
+            "point": {
+                "x": 1,
+                "y": 6,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 4,
+            "point": {
+                "x": 2,
+                "y": 6,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 0
+            }
+        },
+        {
+            "row": 5,
+            "point": {
+                "x": 3,
+                "y": 6,
+                "t": 0,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 6,
+            "point": {
+                "x": 3,
+                "y": 5,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 7,
+            "point": {
+                "x": 3,
+                "y": 4,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 8,
+            "point": {
+                "x": 2,
+                "y": 4,
+                "t": 0,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 9,
+            "point": {
+                "x": 2,
+                "y": 3,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 10,
+            "point": {
+                "x": 1,
+                "y": 3,
+                "t": 0,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 11,
+            "point": {
+                "x": 0,
+                "y": 3,
+                "t": 0,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 12,
+            "point": {
+                "x": 0,
+                "y": 2,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 13,
+            "point": {
+                "x": 0,
+                "y": 1,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 0
+            }
+        },
+        {
+            "row": 14,
+            "point": {
+                "x": 1,
+                "y": 1,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 0
+            }
+        },
+        {
+            "row": 15,
+            "point": {
+                "x": 2,
+                "y": 1,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 0
+            }
+        },
+        {
+            "row": 16,
+            "point": {
+                "x": 3,
+                "y": 1,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 17,
+            "point": {
+                "x": 3,
+                "y": 0,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 0
+            }
+        },
+        {
+            "row": 18,
+            "point": {
+                "x": 4,
+                "y": 0,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 0
+            }
+        },
+        {
+            "row": 19,
+            "point": {
+                "x": 4,
+                "y": 1,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 20,
+            "point": {
+                "x": 4,
+                "y": 2,
+                "t": 0,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 21,
+            "point": {
+                "x": 3,
+                "y": 2,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 22,
+            "point": {
+                "x": 3,
+                "y": 3,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 23,
+            "point": {
+                "x": 4,
+                "y": 4,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 0
+            }
+        },
+        {
+            "row": 24,
+            "point": {
+                "x": 4,
+                "y": 5,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 25,
+            "point": {
+                "x": 4,
+                "y": 6,
+                "t": 0,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 26, //box
+            "point": {
+                "x": 2,
+                "y": 2,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 27,
+            "point": {
+                "x": 1,
+                "y": 2,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 28,
+            "point": {
+                "x": 2,
+                "y": 7,
+                "t": 0,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 29,
+            "point": {
+                "x": 1,
+                "y": 7,
+                "t": 1,
+                "l": 1,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 30,
+            "point": {
+                "x": 1,
+                "y": 8,
+                "t": 1,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 31,
+            "point": {
+                "x": 1,
+                "y": 9,
+                "t": 0,
+                "l": 1,
+                "r": 0,
+                "b": 1
+            }
+        },
+        {
+            "row": 32, //goal
+            "point": {
+                "x": 0,
+                "y": 9,
+                "t": 0,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 33,
+            "point": {
+                "x": 0,
+                "y": 8,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 34,
+            "point": {
+                "x": 0,
+                "y": 7,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        },
+        {
+            "row": 35,
+            "point": {
+                "x": 0,
+                "y": 6,
+                "t": 1,
+                "l": 0,
+                "r": 1,
+                "b": 1
+            }
+        }
+    ]
+};
+// var person, box, goal;
+// function input(person, box, goal){
+//     person = "x=" + json.listPoints[person].point.x + ",y=" + json.listPoints[person].point.y;
+//     box = "x=" + json.listPoints[box].point.x + ",y=" + json.listPoints[box].point.y;
+//     goal = "x=" + json.listPoints[goal].point.x + ",y=" + json.listPoints[goal].point.y;
+//     return "person:" + person + " box:" + box + " goal:" +goal;
+// }
+
+var player = [0,2];
+var box = [2,2];
+var goal = [0,9];
+var row, col;
+var hChoice;
+var cost;
+
+function coordinate(row, col) {
+    this.row = row;
+    this.col = col;
+}
+
+
+function checkExits(json, point) {
+    for (var i in json.listPoints) {
+        if(point[0] == json.listPoints[i].point.x && point[1] == json.listPoints[i].point.y)
+            return true;
+    }
+    return false;
+}
+
+console.log(checkExits(json, player));
+
+
+function manhattan(pos0, pos1){
+    var d1 = Math.abs(pos1.row - pos0.row);
+    var d2 = Math.abs(pos1.col - pos0.col);
+    return d1 + d2;
+}
+function calculate(s, method) {
+    var sum = 0;
+    this.person = state.person;
+    sum += getDist(person, box, method);
+    sum += getDist(box, goal, method);
+    return sum;
+}
+function getDist(obj, sets, method) {
+    var minDist = 1000000;
+    for(var c in sets){
+        var dist;
+        if(method == "m"){
+            dist = manhattan(obj, c);
+        }
+        if(dist < minDist){
+            minDist = dist;
+        }
+    }
+    return minDist;
+}
+function getHeuristic(state) {
+    if (hChoice == "m"){
+        return calculate(state, "m");
+    }
+}
+
+
+var walls;
+var initialState;
+function problem(walls, initialState, goal) {
+    this.walls = walls;
+    this.initialState = initialState;
+    this.goal = goal;
+}
+function goalTest(state) {
+    if (!goal.contains(box)){
+        return false;
+    }
+    return true;
+}
+
+function wallsContain(json, row, col) {
+    for (var i in json.listPoints) {
+        if(row == json.listPoints[i].point.x && col == json.listPoints[i].point.y)
+            return false;
+    }
+    return true;
+}
+function goalContain(goal, row, col) {
+    if (goal[0] == row && goal[1] == col) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function deadlockTest(state) {
+    row = box[0];
+    col = box[1];
+    if (!goalContain(goal, row, col)) {
+        if (wallsContain(json, row-1, col)&&wallsContain(json, row, col-1))
+            return true; //top & left
+        if (wallsContain(json, row-1, col)&&wallsContain(json, row, col+1))
+            return true; //top & right
+        if (wallsContain(json, row+1, col)&&wallsContain(json, row, col-1))
+            return true; //bottom & left
+        if (wallsContain(json, row+1, col)&&wallsContain(json, row, col+1))
+            return true; //bottom & right
+
+        if (wallsContain(json, row-1, col-1)&&wallsContain(json, row-1, col)&&
+            wallsContain(json, row-1, col+1)&&wallsContain(json, row, col-2)&&
+            wallsContain(json, row, col+2)&&(!goalContain(goal, row, col-1))&&
+            !goalContain(goal, row, col+1))
+            return true; //top & sides
+        if (wallsContain(json, row+1, col-1)&&wallsContain(json, row+1, col)&&
+            wallsContain(json, row+1, col+1)&&wallsContain(json, row, col-2)&&
+            wallsContain(json, row, col+2)&&(!goalContain(goal, row, col-1))&&
+            (!goalContain(goal, row, col+1)))
+            return true; //bottom & sides
+        if (wallsContain(json, row-1, col-1)&&wallsContain(json, row, col-1)&&
+            wallsContain(json, row+1, col-1)&&wallsContain(json, row-2, col)&&
+            wallsContain(json, row+2, col)&&(!goalContain(goal, row-1, col))&&
+            (!goalContain(goal, row+1, col)))
+            return true; //left & vertical
+        if (wallsContain(json, row-1, col+1)&&wallsContain(json, row, col+1)&&
+            wallsContain(json, row+1, col+1)&&wallsContain(json, row-2, col)&&
+            wallsContain(json, row+2, col)&&(!goalContain(goal, row-1, col))&&
+            (!goalContain(goal, row+1, col)))
+            return true; //right & top/bottom
+    }
+    return false;
+}
+
